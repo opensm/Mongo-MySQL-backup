9e494739914db470f01d27c3b2682dce13b09aa9
engine-oper <smxblysq@qq.com>
2021-04-26 22:02:35 +0800

    code rsync

--- list_js_mongo.py
+++ list_js_mongo.py
@@ -1,7 +1,8 @@
 from lib.lftp import FTPBackupForDB
+import sys,json
 
 if __name__ == '__main__':
     f = FTPBackupForDB(db='mongo')
     f.connect()
     data = f.show_list(path='dev')
-    print({"list": data})
+    print(json.dumps({"list": data}))
